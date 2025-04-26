efce3ca223fa2f16bc1a9c92948cdf754ce14901
maiphong2004 <maiphong16102004@gmail.com>
2025-04-26 14:41:29 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'; // Đảm bảo import React
 import './App.css';
+import logo from './Group 1.png';
 
 function App() {
   return (
     <div className="App">
+      <div className="top-bar">
+        <div className="logo">
+          <img src={logo} alt="Logo" style={{ height: '60px' }} />
+        </div>
+        <div className="auth-buttons">
+          <button
+            style={{
+              backgroundColor: 'transparent', color: '#ffc107', fontWeight: 'bold'
+            }}>Đăng nhập</button>
+          <button
+            style={{
+              backgroundColor: '#ffc107', color: '#000', fontWeight: 'bold'
+            }}>Đăng ký</button>
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          ✨QUẢN LÝ KHÁCH SẠN DỄ DÀNG, HIỆU QUẢ VÀ THÔNG MINH✨
+          <h5>
+            Tối ưu hoá quy trình đặt phòng, kiểm soát hoạt động và nâng cao trải nghiệm khách hàng - tất cả trong một nền tảng duy nhất
+          </h5>
+        </h3>
       </header>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RootApp from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
 
